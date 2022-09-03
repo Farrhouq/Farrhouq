caec3b3673451e285a8435d98f5a8841d826dc0b
Farouq <farouqimoro@gmail.com>
2022-09-03 13:55:08 +0000

    Restoring my code

--- base/forms.py
+++ base/forms.py
@@ -1,16 +1,10 @@
-from dataclasses import fields
 import imp
 from django.forms import ModelForm
 from .models import Item
-from django.contrib.auth.models import User
 
 class AddItem(ModelForm):
     class Meta:
         model = Item
-        exclude = ['owner']
+        fields = '__all__'
 
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
 
